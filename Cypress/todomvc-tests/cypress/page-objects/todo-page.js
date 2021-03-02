65cd2f2c4c2486da22aaa3937d76ac1e9e7b7428
Andreia Freitas <andgofreitas@gmail.com>
2021-03-02 16:11:01 +0000

    Cypress: - Page Objects in Cypress; - Visual Validation: Screenshots with Applitools Eyes(Passed and Unresolved tests)

--- Cypress/todomvc-tests/cypress/page-objects/todo-page.js
+++ Cypress/todomvc-tests/cypress/page-objects/todo-page.js
@@ -1,49 +1,48 @@
-export class TodoPage {
-    navigate() {
+
+    export function navigate() {
         cy.visit('http://todomvc-app-for-testing.surge.sh/')
     }
 
-    addTodo(todoText) {
+    export function addTodo(todoText) {
         cy.get('.new-todo').type(todoText + '{enter}')
     }
 
-    toggleTodo(todoIndex) {
+    export function toggleTodo(todoIndex) {
         cy.get(`.todo-list li:nth-child(${todoIndex + 1}) .toggle`).click()
     }
 
-    showOnlyCompletedTodos() {
+    export function showOnlyCompletedTodos() {
         cy.contains('Completed').click()
     }
 
-    showOnlyActiveTodos() {
+    export function showOnlyActiveTodos() {
         cy.contains('Active').click()
     }
 
-    showAllTodos() {
+    export function showAllTodos() {
         cy.contains('All').click()
     }
 
-    clearCompleted() {
+    export function clearCompleted() {
         cy.contains('Clear completed').click()
     }
 
-    validateNumberOfTodosShown(expectedNumberOfTodos) {
+    export function validateNumberOfTodosShown(expectedNumberOfTodos) {
         cy.get('.todo-list li').should('have.length', expectedNumberOfTodos)
     }
 
-    validateTodoCompletedState(todoIndex, shouldBeCompleted) {
+    export function validateTodoCompletedState(todoIndex, shouldBeCompleted) {
         const l = cy.get(`.todo-list li:nth-child(${todoIndex + 1}) label`)
 
         l.should(`${shouldBeCompleted ? '' : 'not.'}have.css`, 'text-decoration-line', 'line-through')
     }
 
-    validateTodoText(todoIndex, expectedText) {
+    export function validateTodoText(todoIndex, expectedText) {
         cy.get(`.todo-list li:nth-child(${todoIndex + 1}) label`).should('have.text', expectedText)
     }
 
-    validateToggleState(todoIndex, shouldBeToggled) {
+    export function validateToggleState(todoIndex, shouldBeToggled) {
         const label = cy.get(`.todo-list li:nth-child(${todoIndex + 1}) label`)
 
         label.should(`${shouldBeToggled ? '' : 'not.'}be.checked`)
     }
-}
